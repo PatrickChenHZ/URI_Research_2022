3683924e4698f8bec72e266ba5641707f24a12c9
Helsinki1 <88404697+Helsinki1@users.noreply.github.com>
2022-11-02 21:29:40 -0400

    ardu timing is good but adding prints messes it up

--- src/sender.cpp
+++ src/sender.cpp
@@ -2,7 +2,6 @@
 #include <esp_now.h>    //quote around local lib names, brackets around pio built in lib names
 #include <WiFi.h>
 #include <SoftwareSerial.h>
-#include <HardwareSerial.h>
 
 #define arduRX   16  // hardware serial (Serial2)
 #define arduTX   17
@@ -38,7 +37,8 @@ TaskHandle_t RecvArduHandle;
 TaskHandle_t SendHandle;
 
 //SoftwareSerial SerialSens;
-SoftwareSerial SerialPynq(pynqRX,pynqTX);
+SoftwareSerial SerialPynq;
+//SoftwareSerial SerialArdu;
 //HardwareSerial SerialPynq(1);
 
 String sensPacket;
@@ -48,10 +48,6 @@ String arduPacket;
 bool arduNew = 0;
 bool pynqNew = 0;
 bool sensNew = 0;
-
-bool arduDone = 1;
-bool pynqDone = 0;
-bool sensDone = 0;
 
 // void RecvSensCode(void *parameters) { 
 //   for(;;){
@@ -89,72 +85,63 @@ bool sensDone = 0;
 
 void RecvPynqCode(void *parameters) {
   for(;;){
-    if(arduDone){
-      arduDone = 0;
-      String str = "";
-      // unsigned long begin = millis();
-      int idx = 0;
+    // String str = "";
+    // int idx = 0;
+    // unsigned long begin = millis();
 
-      char c;
-      while(SerialPynq.available()){
-        c = SerialPynq.read();
-        Serial.print(c);
-        //ets_delay_us(25);
-        str += c;
-        if(c=='T'){
-          // ets_delay_us(5);
-          if(str[idx-1]=='%'){
-            pynqPacket = str;
-            break;
-          }
-        }
-        idx++;
-      }
-      pynqNew = 1;
-      pynqDone = 1;
-      //Serial.println("saved: " + str);
-      // Serial.println("chars read: " + String(idx));
-      // Serial.println(millis()-begin);
-    } else{
-      vTaskDelay(1 / portTICK_PERIOD_MS);
-    }
-  }
+    // char c;
+    // while (SerialPynq.available()>0) {
+    //   c = (char)SerialPynq.read();
+    //   str += c;
+    //   if(c=='T'){
+    //     if(str[idx-1]=='%'){
+    //       pynqPacket = str;
+    //       pynqNew = 1;
+    //       Serial.println(pynqPacket);
+    //       Serial.println("str collected... - pynq");
+    //       break;
+    //     }
+    //   }
+    //   idx++;
+    // }
+
+    // Serial.println("READING FINISHED... - pynq");
+    // Serial.println("chars read: " + String(idx));
+    // Serial.println(millis()-begin);
+    vTaskDelay(30 / portTICK_PERIOD_MS);
+  } 
 }
 
 void RecvArduCode(void *parameters) {
-    for(;;){
-    if(pynqDone){
-      pynqDone = 0;
-      String str = "";
-      // unsigned long begin = millis();
-      int idx = 0;
+  for(;;){
+    String str = "";
+    int idx = 0;
+    char c;
+    // unsigned long begin = millis();
 
-      char c;
-      while(Serial2.available()){
-        c = Serial2.read();
-        Serial.print(c);
-        str += c;
-        if(c=='T'){
-          // ets_delay_us(5);
-          if(str[idx-1]=='%'){
-            arduPacket = str;
-            break;
-          }
+    while(Serial2.available()){
+      c = Serial2.read();
+      Serial.print(c);
+      str += c;
+      if(c=='T'){
+        // ets_delay_us(5);
+        if(str[idx-1]=='%'){
+          arduPacket = str;
+          Serial.println("termination detected - ardu");
+          vTaskDelay(26 / portTICK_PERIOD_MS);
+          break;
         }
-        idx++;
       }
-      arduNew = 1;
-      //sensDone = 1;
-      arduDone = 1;
-      //Serial.println("saved: " + str);
-      // Serial.println("chars read: " + String(idx));
-      // Serial.println(millis()-begin);
-    } else{
-      vTaskDelay(1 / portTICK_PERIOD_MS);
+      idx++;
+      //ets_delay_us(53); // perfect delay 87ms
     }
+
+    Serial.println("READING FINISHED... - ardu");
+    // Serial.println("chars read: " + String(idx));
+    // Serial.println(millis()-begin);
+    vTaskDelay(1 / portTICK_PERIOD_MS); // avg interval is 120ms, i wouldnt trust the logic analyzer over the terminal tho
   }
 }
-
 
 unsigned long arduTime = millis();
 unsigned long pynqTime = millis(); //makes sure packet processing doesnt collide
@@ -170,7 +157,6 @@ int count = 0;
 // String pynqPackets[3] = {clear,clear,clear};
 // String sensPackets[3] = {clear,clear,clear};
 int divs;
-
 void SendCode(void *parameters) {
 
   for(;;){
@@ -389,27 +375,31 @@ void SendCode(void *parameters) {
       // vTaskDelay(25 / portTICK_PERIOD_MS);
     }
     */
-    vTaskDelay(5 / portTICK_PERIOD_MS);
+    vTaskDelay(1 / portTICK_PERIOD_MS);
   }
 }
 
 
 void setup() {
   Serial.begin(115200); 
-  Serial2.begin(115200,SERIAL_8N1,16,17);
-  //Serial2.setRxBufferSize(6*packetSize);     // the default buffer size of 64 is too small
-  //SerialPynq.begin(115200,SWSERIAL_8N1,pynqRX,pynqTX,false,128,0);    // try getting the input char by char and see if readstruntil is actually the issue
-  SerialPynq.begin(115200);
-  //SerialSens.begin(115200,SWSERIAL_8N1,sensRX,sensTX,false,128,0);
+  Serial2.setRxBufferSize(512);
+  Serial2.begin(115200,SERIAL_8N1,arduRX,arduTX);
+  //SerialArdu.begin(115200,SWSERIAL_8N1,arduRX,arduTX);
+  SerialPynq.begin(115200,SWSERIAL_8N1,pynqRX,pynqTX);
+  //Serial1.begin(115200,SERIAL_8N1,pynqRX,pynqTX);
+  //SerialSens.begin(115200,SWSERIAL_8N1,sensRX,sensTX);
 
-  Serial2.setTimeout(40);
-  SerialPynq.setTimeout(40);
-  //SerialSens.setTimeout(40);
+  pinMode(pynqRX, INPUT);
+  pinMode(pynqTX, OUTPUT);
+
+  //SerialArdu.setTimeout(3);
+  SerialPynq.setTimeout(1);
+  //SerialSens.setTimeout(5);
 
   //xTaskCreatePinnedToCore(RecvSensCode, "receiving sens packets", 10000, NULL, 3, &RecvSensHandle, 0);
-  xTaskCreatePinnedToCore(RecvPynqCode, "receiving pynq packets", 10000, NULL, 5, &RecvPynqHandle, 0);
-  xTaskCreatePinnedToCore(RecvArduCode, "receiving ardu packets", 10000, NULL, 2, &RecvArduHandle, 0);
-  xTaskCreatePinnedToCore(SendCode, "sending packets", 100000, NULL, 1, &SendHandle, 1);
+  xTaskCreatePinnedToCore(RecvPynqCode, "receiving pynq packets", 30000, NULL, 3, &RecvPynqHandle, 1);
+  xTaskCreatePinnedToCore(RecvArduCode, "receiving ardu packets", 30000, NULL, 3, &RecvArduHandle, 1);
+  xTaskCreatePinnedToCore(SendCode, "sending packets", 100000, NULL, 1, &SendHandle, 0);
 
   //esp_now_set_wake_window(3000); // to increase connection alive time
   WiFi.mode(WIFI_STA);
